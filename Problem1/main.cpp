8a0b5042d0d50fb3c636c8ce93151b0cb052c447
Gokul G <abc@def.com>
2013-04-18 01:23:25 -0700

    Solution to P8, P9, P10

--- Problem1/main.cpp
+++ Problem1/main.cpp
@@ -90,7 +90,8 @@ bool isPrime(long long n)
 {
     if (n <= 2)
         return true;
-    for (long long i = 2; i <= sqrt(n); i++)
+    double sq = sqrt(n);
+    for (long long i = 2; i <= sq; i++)
     {
         if (n % i == 0)
             return false;
@@ -260,7 +261,97 @@ void Prob7()
 
 void Prob8()
 {
+    //Find largest product of three consecutive numbers in the sequence below
+    string str = 
+"73167176531330624919225119674426574742355349194934\
+96983520312774506326239578318016984801869478851843\
+85861560789112949495459501737958331952853208805511\
+12540698747158523863050715693290963295227443043557\
+66896648950445244523161731856403098711121722383113\
+62229893423380308135336276614282806444486645238749\
+30358907296290491560440772390713810515859307960866\
+70172427121883998797908792274921901699720888093776\
+65727333001053367881220235421809751254540594752243\
+52584907711670556013604839586446706324415722155397\
+53697817977846174064955149290862569321978468622482\
+83972241375657056057490261407972968652414535100474\
+82166370484403199890008895243450658541227588666881\
+16427171479924442928230863465674813919123162824586\
+17866458359124566529476545682848912883142607690042\
+24219022671055626321111109370544217506941658960408\
+07198403850962455444362981230987879927244284909188\
+84580156166097919133875499200524063689912560717606\
+05886116467109405077541002256983155200055935729725\
+71636269561882670428252483600823257530420752963450";
+    int max = 0;
+    vector<int> pairs;
+    vector<int> tuples;
+    vector<int> quads;
     
+    int i = 0;
+    while ((i+1) < str.length())
+    {
+        int a = (int) (str.at(i) - '0');
+        int b = (int) (str.at(i+1) - '0');
+        pairs.push_back(a*b);
+        i+=1;
+    }
+    //printf("Pairs : %d\n", pairs.size());
+    for (int j = 2; j < 5; j++) {
+        for (i = j; i < str.length(); i++) {
+            int val = (int) (str.at(i) - '0');
+            int lindex = i - j;
+            int cnt = 0;
+            switch (j)
+            {
+                case 2: cnt = pairs.at(lindex); break;
+                case 3: cnt = tuples.at(lindex); break;
+                case 4: cnt = quads.at(lindex); break;
+            }
+            int prod = val * cnt;
+            switch (j)
+            {
+                case 2:  tuples.push_back(prod); break;
+                case 3:  quads.push_back(prod); break;
+                case 4:
+                    if (prod > max)
+                        max = prod;
+                    break;
+            }
+        }
+    }
+    
+    printf("Prob8 Soln : Largest Prod %d\n", max);
+}
+
+void Prob9()
+{
+    //Find Pythagorean Triplet a<b<c such that a^2 + b^2 = c^2 and a + b + c = 1000, find abc
+    for (int b = 1; b < 1000; b++)
+    {
+        for (int c = b+1; c < 1000; c++)
+        {
+            if ((1000*(b+c) - (b*b) - (b*c)) == 5*pow(10,5))
+            {
+                printf ("Prob9 Soln : abc = %d :: a = %d; b = %d; c = %d\n", (b*c*(1000-b-c)), (1000-b-c), b, c);
+                return;
+            }
+        }
+    }
+}
+
+void Prob10()
+{
+    uint64_t sum = 0;
+    for (int i = 2; i < 2000000; i++)
+    {
+        if (isPrime(i))
+        {
+            sum += (uint64_t) i;
+            printf("%d\n", i);
+        }
+    }
+    printf("Prob10 Soln : Sum of Primes = %lld\n", sum);
 }
 
 int main(int argc, char** argv) {
@@ -272,6 +363,8 @@ int main(int argc, char** argv) {
     Prob6();
     Prob7();
     Prob8();
+    Prob9();
+    Prob10();
     return 0;
 }
 
